4446f9aeac6b59041ab1bf4c69f8ebfdd67fc63f
devbxtzz <batztonnie@gmail.com>
2022-12-22 16:50:03 +0300

    ES6 Classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,20 +2,12 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    // call constructor of super class (Building)
     super(brand, motor, color);
-
-    // Create objs
     this._range = range;
   }
 
- 
-
-  cloneCar() {
-    const NewObj = this.constructor[Symbol.species] || this.constructor;
-    const clone = new NewObj();
-    return clone;
+  // eslint-disable-next-line
+	cloneCar() {
+    return new Car[Symbol.species]();
   }
-
-  
 }
